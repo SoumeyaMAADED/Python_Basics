504e5633f9595e313beafdf03dc51c0d69cc0232
Soumeya <s.maaded@gmail.com>
2024-10-03 22:54:51 +0200

    Initial commit

---
+++ 01. First Steps In Coding/01. Lab/01. HelloSoftuni.py
@@ -0,0 +1,9 @@
+#  Python est un langage de programmation puissant et facile à apprendre. Il possède des structures de données efficaces de haut niveau et une approche simple mais efficace de la programmation orientée objet. La syntaxe élégante et le typage dynamique de Python, ainsi que sa nature interprétée, en font un langage idéal pour la création de scripts et le développement rapide d'applications dans de nombreux domaines sur la plupart des plateformes.
+
+# L'interpréteur Python et la vaste bibliothèque standard sont disponibles gratuitement sous forme source ou binaire pour toutes les principales plates-formes sur le site Web Python, https://www.python.org/, et peuvent être distribués gratuitement. Le même site contient également des distributions et des pointeurs vers de nombreux modules, programmes et outils Python tiers gratuits, ainsi qu'une documentation supplémentaire.
+
+# L'interpréteur Python est facilement étendu avec de nouvelles fonctions et types de données implémentés en C ou C++ (ou d'autres langages appelables depuis C). Python convient également comme langage d'extension pour les applications personnalisables. 
+
+# Écrivez le code de programme suivant (commande pour imprimer le texte « Hello SoftUni ») : 
+
+print('HelloSoftUni')
---
+++ 01. First Steps In Coding/01. Lab/02. Nums 1...10.py
@@ -0,0 +1,24 @@
+# Écrivez  un  programme de console Python qui imprime les chiffres de 1 à 10 sur des lignes distinctes sur la console.
+
+# Exemple 1:
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+# Exemple 2:
+
+for i in range (10):
+    print(1+i)
+
+# Utilisation de la compréhension de liste et d'une boucle
+
+x = [i for i in range(1, 11)]
+print(x)
