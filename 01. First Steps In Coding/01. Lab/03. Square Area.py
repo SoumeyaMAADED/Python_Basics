dde840a71e69a433559d250d8c33327f6ae6981d
Soumeya <s.maaded@gmail.com>
2025-01-01 22:44:42 +0100

    Pythonesque

---
+++ 01. First Steps In Coding/01. Lab/03. Square Area.py
@@ -0,0 +1,22 @@
+# Écrivez un programme console qui entre deux entiers (les côtés du rectangle a et b) et calcule la face d'un 
+# rectangle avec ces côtés. 
+ 
+# Initialisez deux variables (a et b) et écrivez-y les valeurs saisies par la console. 
+# Initialisez une deuxième aire variable pour écrire la valeur de la face du rectangle obtenue par la formule a * b. 
+# Imprimez le résultat.
+
+# Dans l'exercice de SoftUni, on écrira plutôt les données dans la console qu'on éxécutera au fur et à mesure (avec le bouton play en haut" Exécuter le fichier python dans le terminal dédié ), afin d'afficher le résultat.
+
+# a = int(input())
+# b = int(input())
+# area = a*b
+
+# print(area)
+
+# Sinon dans la règle, nous écrivons plutôt ça:
+
+a = int(5)
+b = int(7)
+area = a*b
+
+print(area)
