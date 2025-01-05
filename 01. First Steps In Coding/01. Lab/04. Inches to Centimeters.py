364ed435333818763c5031429fb7d128caeb251b
Soumeya <s.maaded@gmail.com>
2025-01-05 22:56:58 +0100

    Suite

---
+++ 01. First Steps In Coding/01. Lab/04. Inches to Centimeters.py
@@ -0,0 +1,9 @@
+def convertisseur(input):
+    inch = 2.54  # Conversion d'un pouce en centimètres
+    centimeters = float(input) * inch  # Conversion de la valeur d'entrée
+    print(centimeters)  # Affichage du résultat
+
+# Appels de la fonction avec différentes valeurs
+
+convertisseur(5)
+convertisseur(7)
