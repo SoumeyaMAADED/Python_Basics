d5a9cd4719d7b88b99711737e6a2e90c61d55a84
Soumeya <s.maaded@gmail.com>
2025-01-07 22:00:21 +0100

    ajout d'une explication détaillé FLOAT

--- 01. First Steps In Coding/01. Lab/04. Inches to Centimeters.py
+++ 01. First Steps In Coding/01. Lab/04. Inches to Centimeters.py
@@ -1,6 +1,10 @@
 def convertisseur(input):
     inch = 2.54  # Conversion d'un pouce en centimètres
-    centimeters = float(input) * inch  # Conversion de la valeur d'entrée
+    centimeters = float(input) * inch  # Conversion de la valeur d'entrée. 
+    
+    # Avec la fonction float, j’ai élargi les possibilités pour que la fonction puisse gérer à la fois des nombres entiers et des nombres à virgule. Cela rend le programme plus flexible et adapté à des cas où l’utilisateur pourrait entrer des valeurs comme 5.5 pouces ou 7.25 pouces.
+    # L’idée est d’anticiper des besoins potentiels afin d'éviter les désagréments du terminal ;).
+
     print(centimeters)  # Affichage du résultat
 
 # Appels de la fonction avec différentes valeurs
