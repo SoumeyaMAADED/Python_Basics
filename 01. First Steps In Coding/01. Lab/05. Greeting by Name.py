e0917245a0a5a203c025947c63856992caf1b668
Soumeya <s.maaded@gmail.com>
2025-01-08 23:19:37 +0100

    Simple example

---
+++ 01. First Steps In Coding/01. Lab/05. Greeting by Name.py
@@ -0,0 +1,4 @@
+# Écrivez un programme qui lit le texte (nom de la personne) à partir de la console et affiche "Bonjour, <nom> !", où <nom> est le nom saisi par la console.
+
+name = input()
+print ('Hello, ' + name + '!')
